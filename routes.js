669e93814adcc889a39cd4b55d191a62ffe6e3cc
Nathan Looney <nal3vm@virginia.edu>
2014-06-25 08:44:20 -0400

    Creating API

--- routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-	var handlers = require('./handlers');
-	app.get('/', handlers.index);
-	app.get('/info', handlers.info);
-	app.get('/test', handlers.test);
-	app.get('/add', handlers.createPost);
-}
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var express = require('express');
+// server.js (Express 4.0)
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+//var methodOverride = require('method-override');
+var app            = express();
+var router = express.Router();
 
-var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+
+app.use(morgan('dev')); 					// log every request to the console
+//app.use(bodyParser()); 						// pull information from html in POST
+//app.use(methodOverride()); 					// simulate DELETE and PUT
+
+router.get('/', function(req, res) {
+	res.send('message: Welcome to LNY/MSC');
 });
 
-/* istanbul ignore next */
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
-}
+app.use('/api', router);
+
+app.listen(8080);	
+console.log('Magic happens on port 8080'); 			// shoutout to the user
