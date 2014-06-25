5dc72574c9436fbd9fa6e64293672b39073bd94b
Nathan Looney <nal3vm@virginia.edu>
2014-06-25 08:48:56 -0400

    added MongoDB

--- server.js
+++ server.js
@@ -6,17 +6,46 @@ var bodyParser     = require('body-parser');
 var app            = express();
 var router = express.Router();
 
+var Post = require('./app/models/post');
+var port = process.env.PORT || 3000;
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL;
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
 
 
 app.use(morgan('dev')); 					// log every request to the console
 //app.use(bodyParser()); 						// pull information from html in POST
 //app.use(methodOverride()); 					// simulate DELETE and PUT
 
-router.get('/', function(req, res) {
-	res.send('message: Welcome to LNY/MSC');
-});
+router.route('/posts')
+	.post(function(req, res) {
+		var post = new Post();
+		post.title = req.body.name;
+		post.save(function(err) {
+			if (err) {
+				res.send(err);
+			}
+			res.json({message: 'Bear created!'});
+		});
+	});
+
+
+
+
+
+
 
 app.use('/api', router);
 
-app.listen(8080);	
-console.log('Magic happens on port 8080'); 			// shoutout to the user
+app.listen(port);	
+console.log('Magic happens on port '+port); 			// shoutout to the user
